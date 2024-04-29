edcf2e04e806390184e274898a9cd0c075a9ec37
yux <34335406+yuxiqian@users.noreply.github.com>
2024-04-29 20:23:44 +0800

    [FLINK-35264][cdc][runtime] Fix multiple transform rules do not take effect (#3280)

--- flink-cdc-e2e-tests/flink-cdc-pipeline-e2e-tests/src/test/java/org/apache/flink/cdc/pipeline/tests/TransformE2eITCase.java
+++ flink-cdc-e2e-tests/flink-cdc-pipeline-e2e-tests/src/test/java/org/apache/flink/cdc/pipeline/tests/TransformE2eITCase.java
@@ -28,6 +28,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
@@ -43,6 +45,7 @@ import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 /** E2e tests for the {@link TransformSchemaOperator}. */
+@RunWith(Parameterized.class)
 public class TransformE2eITCase extends PipelineTestEnvironment {
     private static final Logger LOG = LoggerFactory.getLogger(TransformE2eITCase.class);
 
