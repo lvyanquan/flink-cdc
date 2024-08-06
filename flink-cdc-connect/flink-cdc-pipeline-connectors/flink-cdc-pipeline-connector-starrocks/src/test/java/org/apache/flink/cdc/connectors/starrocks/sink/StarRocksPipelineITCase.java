41ec6300e00ac88601eaba758016db8dd26ac5b0
gongzhongqiang <764629910@qq.com>
2024-08-06 21:47:43 +0800

    Revert "[hotfix][starrocks] Fix StarRocks FE startup failure due to insufficient disk space available "
    This reverts commit 3315be3c8fbbdc88947e162335674ee544ae7604.

--- flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-starrocks/src/test/java/org/apache/flink/cdc/connectors/starrocks/sink/StarRocksPipelineITCase.java
+++ flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-starrocks/src/test/java/org/apache/flink/cdc/connectors/starrocks/sink/StarRocksPipelineITCase.java
@@ -44,7 +44,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.apache.flink.cdc.connectors.starrocks.sink.StarRocksDataSinkOptions.JDBC_URL;
@@ -165,10 +164,6 @@ public class StarRocksPipelineITCase extends StarRocksSinkTestBase {
                         .set(JDBC_URL, STARROCKS_CONTAINER.getJdbcUrl())
                         .set(USERNAME, StarRocksContainer.STARROCKS_USERNAME)
                         .set(PASSWORD, StarRocksContainer.STARROCKS_PASSWORD);
-
-        config.addAll(
-                Configuration.fromMap(
-                        Collections.singletonMap("table.create.properties.replication_num", "1")));
 
         Sink<Event> starRocksSink =
                 ((FlinkSinkProvider) createStarRocksDataSink(config).getEventSinkProvider())
